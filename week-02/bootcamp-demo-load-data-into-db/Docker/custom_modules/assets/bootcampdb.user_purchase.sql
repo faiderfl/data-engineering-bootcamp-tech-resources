f568ad8b255653e4fc71f7ae6a8f5e05d1e24efc
ffvalencia <faiderfl@outlook.com>
2021-10-31 20:40:27 -0500

    Add user_purchase

---
+++ week-02/bootcamp-demo-load-data-into-db/Docker/custom_modules/assets/bootcampdb.user_purchase.sql
@@ -0,0 +1,16 @@
+-- Table: bootcampdb.user_purchase
+
+DROP TABLE bootcampdb.user_purchase;
+
+
+CREATE TABLE IF NOT EXISTS bootcampdb.user_purchase
+        (
+            invoice_number varchar(10),
+            stock_code varchar(20),
+            detail varchar(1000),
+            quantity int,
+            invoice_date timestamp,
+            unit_price numeric(8,3),
+            customer_id int,
+            country varchar(20)
+        );
